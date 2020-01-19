f68215d4d02ab24ce76eceb0255e4f1845888e1d
Tomo Zaidem <tomozaidem@gmail.com>
2020-01-19 22:37:09 +0800

    added sign in and sign up page, sign in component, form input component and custom button component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,7 +12,7 @@ const Header = () => (
         </Link>
         <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
-            <Link className='option' to='/shop'>CONTACT</Link>
+            <Link className='option' to='/signin'>CONTACT</Link>
         </div>
     </div>
 )
